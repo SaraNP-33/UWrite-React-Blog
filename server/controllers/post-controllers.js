f4ebe354f2ea1353f86c3d54c6bff11c705de80d
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-12-19 18:14:59 -0100

    created post controllers to go with the route posts

---
+++ server/controllers/post-controllers.js
@@ -0,0 +1,47 @@
+const {Post} = require('../models');
+
+const postController = {
+    async createPost (req, res) {
+        try {
+            const postData = await Post.create(req.body);
+            res.status(200).json(postData);
+        } catch (err) {
+            console.log(err);
+            res.status(400).json(err);
+        }
+    },
+    async updatePost (req, res) {
+        try {
+            const postData = await Post.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            });
+            if (!postData) {
+                res.status(404).json({ message: 'No post found with this id!' });
+                return;
+            }
+            res.status(200).json(postData);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    async deletePost (req, res) {
+        try {
+            const postData = await Post.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+            if (!postData) {
+                res.status(404).json({ message: 'No post found with this id!' });
+                return;
+            }
+            res.status(200).json(postData);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    }
+}
+
+module.exports = postController;
