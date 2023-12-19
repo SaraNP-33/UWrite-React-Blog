b16083786b9fc7febd8e2afd5f0850d724ff7b3c
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2023-12-19 18:13:40 -0100

    added the routes for posts and they are working

--- server/routes/api/post-routes.js
+++ server/routes/api/post-routes.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { authMiddleware } = require('../../utils/auth');
+
+const {createPost,updatePost,deletePost} = require('../../controllers/post-controllers');
+
+router.route('/').post(authMiddleware,createPost);
+router.route('/:id').put(authMiddleware,updatePost).delete(authMiddleware,deletePost);
+
+module.exports = router;
